e48e978a4b4b19b31a8cc8d24730f0379c830a7c
Alec Johnson <alecjohnson55@gmail.com>
2017-06-21 16:17:05 -0700

    initial commit

---
+++ webpage_cleaner.py
@@ -0,0 +1,9 @@
+# Our team has just discovered that search engines are negatively scoring
+# our web-pages that include a link to the domain shittylistings.com. For
+# simplicity assume that we have 50,000 HTML files in a Unix directory tree
+# under a folder called "/website" (e.g. /website/home.html,
+# /website/san-diego/2505-via-clarita.html, etc). We have 24 hours to
+# get a list of file paths to the editorial staff so they can
+# edit the pages and remove the links. You need to make a list of the
+# .html files that contain links to the domain. How would you
+# solve the problem? Keep in mind our team is on a short deadline. 
