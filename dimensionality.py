6c182d68147c6a622b4bb50098e0c4cdf4438ae5
Alec Johnson <alecjohnson55@gmail.com>
2017-06-21 23:21:55 -0700

    turn statement into multi-line comment, comment out test cases

--- dimensionality.py
+++ dimensionality.py
@@ -1,13 +1,13 @@
-# the dimension of an array is defined as the number
-# of indices needed to select an element. (e.g. [] has 1
-# dimension, [[1]] has 2, [[2], [2, [3]]]) has 3 dimensions).
-# The programming language you are using doesn't implement this as
-# part of its standard library. Implement a method that determines
-# the dimensionality of a given array. How performant is this function?
+"""the dimension of an array is defined as the number
+of indices needed to select an element. (e.g. [] has 1
+dimension, [[1]] has 2, [[2], [2, [3]]]) has 3 dimensions).
+The programming language you are using doesn't implement this as
+part of its standard library. Implement a method that determines
+the dimensionality of a given array. How performant is this function?
 
-# this is essentially finding the depth of a BST, so we can treat it as
-# such. If the current "node" is not an array, then we are at the deepest
-# point of that "tree" and thus return 0. Otherwise recurse through it.
+this is essentially finding the depth of a BST, so we can treat it as
+such. If the current "node" is not an array, then we are at the deepest
+point of that "tree" and thus return 0. Otherwise recurse through it."""
 
 def dimensionality(array):
     # the dimensionality of anything but an array is 0
@@ -20,13 +20,13 @@ def dimensionality(array):
     else:
         return 1 + max(dimensionality(item) for item in array)
 
-print(dimensionality([]))
-print(dimensionality([1]))
-print(dimensionality([[2]]))
-print(dimensionality([[2], [2, [3]]]))
-print(dimensionality([[[[[[1]]]]]]))
-print(dimensionality(['a']))
-print(dimensionality([['a', ['b']]]))
+# print(dimensionality([]))
+# print(dimensionality([1]))
+# print(dimensionality([[2]]))
+# print(dimensionality([[2], [2, [3]]]))
+# print(dimensionality([[[[[[1]]]]]]))
+# print(dimensionality(['a']))
+# print(dimensionality([['a', ['b']]]))
 
-# for the performance of the function it is O(n) where n
+# the performance of the function is O(n) where n
 # is the number of elements.
